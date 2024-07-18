32f9b96cfff84633ad2d8fa8a4f6531000aeac05
LSD <luciferiidem@gmail.com>
2024-07-18 12:31:02 +0700

    fixing problems

--- newfile.py
+++ newfile.py
@@ -1,65 +1,60 @@
 import requests
 import os
 import time
-from config import access_token, token_api
 from datetime import datetime, date
 from tqdm import tqdm
 
 
-class VK:
-    def __init__(self, access_token, api_token, version='5.199'):
-
-        self.token = access_token
-        self.token_api = api_token
-        self.version = version
-        self.params = {'access_token': self.token, 'v': self.version}
-
-    def vk_fotos(self, user_id):
-        n = 0
-        url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': user_id, 'access_token': self.token, 'rev': 0, 'count': 5,
-                  'extended': 1, 'album_id': 'profile', 'photo_sizes': True, 'v': self.version}
-        response = requests.get(url, params=params)
-        for el in response.json()['response']['items']:
-            for size in el['sizes']:
-                if size['type'] == 'm' and el['likes']['count'] != 0:
-                    url_image = size['url']
-                    file_name = f'{el['likes']['count']}.jpg'
-                    response = requests.get(url_image)
-                    with open(f'image//{file_name}', 'wb') as f:
-                        f.write(response.content)
-                        time.sleep(0.5)
-                elif size['type'] == 'z' and el['likes']['count'] == 0:
-                    n += 1
-                    url_image = size['url']
-                    file_name = f'{el['likes']['count']}_{
-                        datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.jpg'
-                    response = requests.get(url_image)
-                    with open(f'Image//{file_name}', 'wb') as f:
-                        f.write(response.content)
-                        time.sleep(0.5)
-
-    def unload_ya(self, file):
-
-        headers = {'Authorization': self.token_api}
-        response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
-                                headers=headers,
-                                params={'path': f'Image/{file}', 'overwrite': True})
-        url_upload = response.json()['href']
-        with open(f'image/{file}', 'rb') as image:
-            response = requests.put(url_upload, files={'file': image})
-
-    def backup(self):
-        vk.vk_fotos(input('Введите ID:'))
-        total = len(os.listdir('Image'))
-        count = 0
-        with tqdm(total=len(os.listdir('Image')), desc="Загрузка файлов...") as pbar:
-            for file in os.listdir('Image'):
-                count += 1
-                pbar.set_description(f"Загружено {count} из {total}")
-                vk.unload_ya(file)
-                pbar.update(100/total)
+def vk_fotos(user_id):
+    photos_info = {}
+    url = 'https://api.vk.com/method/photos.get'
+    params = {'owner_id': user_id, 'access_token': 'введите токен',
+              'rev': 0, 'count': 5, 'extended': 1, 'album_id': '303306260', 'photo_sizes': True, 'v': '5.199'}
+    response = requests.get(url, params=params)
+    for el in response.json()['response']['items']:
+        for size in el['sizes']:
+            if size['type'] == 'm' and el['likes']['count']not in photos_info:
+                url_image = size['url']
+                like = el['likes']['count']
+                file_name = f'{like}.jpg'
+                photos_info[like] = {"url": url_image, "size": size['type']}
+                response = requests.get(url_image)
+                with open(f'image//{file_name}', 'wb') as f:
+                    f.write(response.content)
+                    time.sleep(0.5)
+            elif size['type'] == 'm' and like in photos_info:
+                url_image = size['url']
+                file_name = f'{like}_{
+                    datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.jpg'
+                photos_info[like] = {"url": url_image, "size": size['type']}
+                response = requests.get(url_image)
+                with open(f'Image//{file_name}', 'wb') as f:
+                    f.write(response.content)
+                    time.sleep(0.5)
 
 
-vk = VK(access_token, token_api)
-vk.backup()
+def unload_ya(file):
+
+    headers = {
+        'Authorization': 'введите токен'}
+    response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                            headers=headers,
+                            params={'path': f'Image/{file}', 'overwrite': True})
+    url_upload = response.json()['href']
+    with open(f'image/{file}', 'rb') as image:
+        response = requests.put(url_upload, files={'file': image})
+
+
+def backup():
+    vk_fotos('101405428')
+    total = len(os.listdir('Image'))
+    count = 0
+    with tqdm(total=len(os.listdir('Image')), desc="Загрузка файлов...") as pbar:
+        for file in os.listdir('Image'):
+            count += 1
+            pbar.set_description(f"Загружено {count} из {total}")
+            unload_ya(file)
+            pbar.update(100/total)
+
+
+backup()
